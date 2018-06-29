c42273dbfc7ec89974b78cb90bea20e485acd4cc
Song Jaehak <master@hpi.cc>
2018-06-29 13:04:32 +0900

    Add upload api without ocr response

--- translate/api.py
+++ translate/api.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+import os
+import uuid
+import PyPDF2
+import io
+
+from flask import Flask, render_template, request
 from flask_restful import Resource, Api
 from flask_restful import reqparse
 from .translate import run_translate
+from google.cloud import vision
+from google.protobuf.json_format import MessageToJson
+
+from wand.image import Image
 
 app = Flask(__name__)
 api = Api(app)
@@ -32,6 +41,61 @@ class Translate(Resource):
 
 
 api.add_resource(Translate, '/translate')
+
+
+def ocr(imagepath, outputpath):
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(imagepath, 'rb') as imagefile:
+        content = imagefile.read()
+
+    image = vision.types.Image(content=content)
+    response = client.document_text_detection(image=image)
+
+    with io.open(outputpath, 'w') as f:
+        f.write(MessageToJson(response))
+
+
+class Upload(Resource):
+    def post(self):
+        pdf = request.files['file']
+        file_id = str(uuid.uuid4())
+
+        dirpath = '/tmp/' + file_id
+        os.makedirs(dirpath)
+
+        pdfpath = os.path.join(dirpath, 'org.pdf')
+
+        pdf.save(pdfpath)
+
+        with open(pdfpath, 'rb') as f:
+            pdf = PyPDF2.PdfFileReader(pdfpath)
+            page_count = pdf.getNumPages()
+            
+            print("page_count:", page_count)
+
+        imgpath = os.path.join(dirpath, 'images')
+        os.makedirs(imgpath)
+
+        for i in range(page_count):
+            print("convert page:", i)
+            name = "{}[{}]".format(pdfpath, i)
+            with Image(filename=name, resolution=100) as image:
+                image.format = 'jpeg'
+                image.compression_quality = 75
+                image.save(filename=os.path.join(imgpath, "{}.jpg".format(i)))
+
+        jsonpath = os.path.join(dirpath, 'json')
+        os.makedirs(jsonpath)
+        for i in range(page_count):
+            print("ocr page:", i)
+            ocr(os.path.join(imgpath, "{}.jpg".format(i)),
+                os.path.join(jsonpath, "{}.json".format(i)))
+
+        return dict(file_id=file_id)
+
+
+api.add_resource(Upload, '/upload')
 
 
 @app.route('/test')
